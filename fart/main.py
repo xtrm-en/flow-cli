e9fec9667b2161f688315a98d6fea18e50f8f225
xtrm <oss@xtrm.me>
2023-06-25 03:00:27 +0200

    🔨 chore(test): inquirer

--- fart/main.py
+++ fart/main.py
@@ -1,5 +1,18 @@
+import sys
+
 from fart.utils import xlog
+import inquirer
 
 
 def main() -> None:
     xlog("Welcome to F.A.R.T.")
+    sys.stdout.flush()
+
+    questions = [
+        inquirer.Checkbox('interests',
+                          message="What are you interested in?",
+                          choices=['Computers', 'Books', 'Science', 'Nature', 'Fantasy', 'History'],
+                          ),
+    ]
+    answers = inquirer.prompt(questions)
+    print(answers['interests'])
