d0eade7dfa37c716f1bc2f4e1186ca9c85348546
xtrm <oss@xtrm.me>
2023-07-02 00:59:18 +0200

    🔨 chore: __all__

--- fart/utils/__init__.py
+++ fart/utils/__init__.py
@@ -2,3 +2,10 @@ from .colors import Colors
 from .inquirer import prompt
 from .log import log, fatal, info, warn, error, success
 from .strings import parse, stringify
+
+__all__ = [
+    "Colors",
+    "prompt",
+    "log", "fatal", "info", "warn", "error", "success",
+    "parse", "stringify",
+]
